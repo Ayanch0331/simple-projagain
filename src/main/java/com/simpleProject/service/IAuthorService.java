9699fce2924ad0545645eeed7d9494f026b79aa5
Ayan Chakraborty <ayan.chakraborty@cognizant.com>
2019-07-11 11:00:58 +0530

    d1

--- src/main/java/com/simpleProject/service/IAuthorService.java
+++ src/main/java/com/simpleProject/service/IAuthorService.java
@@ -7,7 +7,7 @@ import com.simpleProject.entity.AuthorEntity;
 public interface IAuthorService {
 
 	List<AuthorEntity> findAllAuthors();
-	//for new branch again
+	//for new branch
 	AuthorEntity createAuthor(AuthorEntity ae);
 	
 }
